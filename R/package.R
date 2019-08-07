7d8002c954949ed47e6df699b0caf0acb826945f
Ming Yang <hustyangming@gmail.com>
2019-08-07 15:59:54 +0800

    update

--- R/package.R
+++ R/package.R
@@ -14,8 +14,23 @@
 #' @import np Rcpp RcppArmadillo
 NULL
 
-#' BS tree algorithm
+#' Objective function minimization
 #' 
-#' @param y a numeric vector
+#' @param coef initial parameter values
+#' @param Z covariates matrix
+#' @param y response vector
+#' @param u a vector of weights
+#' @param v a vector of weigths 
+#' @param tree indicator of whether the tree algorithm should be used for fast ranking
+#' @param nrep number of replications in the optimization algorithm
+#' @param ftol function tolerance to determine convergence
+#' @param nfun_max maximum number of function evaluations
+#' 
 #' @export
-bs.tree <- function(y) bs_tree(y)
+obj.min <- function(coef, Z, y, u = NULL, v = NULL, tree = TRUE, 
+                    nrep = 1, ftol = 1e-6, nfun_max = 5000) {
+  n <- length(y)
+  if (is.null(u)) u <- rep(1, n)
+  if (is.null(v)) v <- rep(1, n)
+  obj_min(coef, Z, y, u, v, tree, nrep, ftol, nfun_max)
+}
