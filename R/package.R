a72daa868e871ce481415fb852d198f3c6129aaa
Ming Yang <hustyangming@gmail.com>
2019-08-07 16:12:04 +0800

    update

--- R/package.R
+++ R/package.R
@@ -14,24 +14,3 @@
 #' @import np Rcpp RcppArmadillo
 NULL
 
-#' Objective function minimization
-#' 
-#' @param coef initial parameter values
-#' @param Z covariates matrix
-#' @param y response vector
-#' @param u a vector of weights
-#' @param v a vector of weigths 
-#' @param tree indicator of whether the tree algorithm should be used for fast ranking
-#' @param nrep number of replications in the optimization algorithm
-#' @param ftol function tolerance to determine convergence
-#' @param nfun_max maximum number of function evaluations
-#' 
-#' @export
-obj.min <- function(coef, Z, y, u = NULL, v = NULL, tree = TRUE, 
-                    nrep = 1, ftol = 1e-6, nfun_max = 5000) {
-  n <- length(y)
-  if (is.null(u)) u <- rep(1, n)
-  if (is.null(v)) v <- rep(1, n)
-  M <- obj_min(coef, Z, y, u, v, tree, nrep, ftol, nfun_max)
-  list(min = M[1, 1], param = M[1, -1])
-}
--- R/srat.R
+++ R/srat.R
@@ -63,3 +63,24 @@ srat <- function(Y, Z, X, cluster = NULL, init = NULL, w_sqrt = NULL,
        Q = Q, Q_ptb = Q_ptb, SIGMA = SIGMA, lambda = lambda, pval = pval)
 }
 
+#' Objective function minimization
+#' 
+#' @param coef initial parameter values
+#' @param Z covariates matrix
+#' @param y response vector
+#' @param u a vector of weights
+#' @param v a vector of weigths 
+#' @param tree indicator of whether the tree algorithm should be used for fast ranking
+#' @param nrep number of replications in the optimization algorithm
+#' @param ftol function tolerance to determine convergence
+#' @param nfun_max maximum number of function evaluations
+#' 
+#' @export
+obj.min <- function(coef, Z, y, u = NULL, v = NULL, tree = TRUE, 
+                    nrep = 1, ftol = 1e-6, nfun_max = 5000) {
+  n <- length(y)
+  if (is.null(u)) u <- rep(1, n)
+  if (is.null(v)) v <- rep(1, n)
+  M <- obj_min(coef, Z, y, u, v, tree, nrep, ftol, nfun_max)
+  list(min = M[1, 1], param = M[1, -1])
+}
